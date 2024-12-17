00770fc8ac2598c57b19e52124cf5303050965c7
hadbet <hadbet.altamirano@grammer.com>
2024-12-17 16:04:55 -0600

    testr

--- dao/consultaReporteFinalDetalles.php
+++ dao/consultaReporteFinalDetalles.php
@@ -13,48 +13,48 @@ function ContadorApu()
     SUM(
         CASE 
             WHEN ISap.Cantidad > COALESCE( 
-                CASE WHEN BInv.TercerConteo != 0 THEN BInv.TercerConteo END, 
-                CASE WHEN BInv.SegundoConteo != 0 THEN BInv.SegundoConteo END, 
+                CASE WHEN BInv.TercerConteo != 0.00 THEN BInv.TercerConteo END, 
+                CASE WHEN BInv.SegundoConteo != 0.00 THEN BInv.SegundoConteo END, 
                 BInv.PrimerConteo 
-            , 0) OR BInv.NumeroParte IS NULL THEN 1
+            , 0.00) OR BInv.NumeroParte IS NULL THEN 1
             ELSE 0
         END
     ) AS 'Cantidad_Total_Negativa',
     SUM(
         CASE 
             WHEN ISap.Cantidad < COALESCE( 
-                CASE WHEN BInv.TercerConteo != 0 THEN BInv.TercerConteo END, 
-                CASE WHEN BInv.SegundoConteo != 0 THEN BInv.SegundoConteo END, 
+                CASE WHEN BInv.TercerConteo != 0.00 THEN BInv.TercerConteo END, 
+                CASE WHEN BInv.SegundoConteo != 0.00 THEN BInv.SegundoConteo END, 
                 BInv.PrimerConteo 
-            , 0) AND BInv.Estatus = 1 THEN 1
+            , 0.00) AND BInv.Estatus = 1 THEN 1
             ELSE 0
         END
     ) AS 'Cantidad_Total_Positiva',
     SUM(
         CASE 
-            WHEN ISap.Cantidad > COALESCE( 
-                CASE WHEN BInv.TercerConteo != 0 THEN BInv.TercerConteo END, 
-                CASE WHEN BInv.SegundoConteo != 0 THEN BInv.SegundoConteo END, 
+            WHEN (ISap.Cantidad * (Parte.Por / Parte.Costo)) > (COALESCE( 
+                CASE WHEN BInv.TercerConteo != 0.00 THEN BInv.TercerConteo END, 
+                CASE WHEN BInv.SegundoConteo != 0.00 THEN BInv.SegundoConteo END, 
                 BInv.PrimerConteo 
-            , 0) OR BInv.NumeroParte IS NULL THEN (ISap.Cantidad - COALESCE( 
-                CASE WHEN BInv.TercerConteo != 0 THEN BInv.TercerConteo END, 
-                CASE WHEN BInv.SegundoConteo != 0 THEN BInv.SegundoConteo END, 
+            , 0.00) * (Parte.Por / Parte.Costo)) AND BInv.Estatus = 1 THEN (ISap.Cantidad * (Parte.Por / Parte.Costo)) - (COALESCE( 
+                CASE WHEN BInv.TercerConteo != 0.00 THEN BInv.TercerConteo END, 
+                CASE WHEN BInv.SegundoConteo != 0.00 THEN BInv.SegundoConteo END, 
                 BInv.PrimerConteo 
-            , 0)) * (Parte.Por / Parte.Costo)
+            , 0.00) * (Parte.Por / Parte.Costo))
             ELSE 0
         END
     ) AS 'Costo_Total_Negativo',
     SUM(
         CASE 
-            WHEN ISap.Cantidad < COALESCE( 
-                CASE WHEN BInv.TercerConteo != 0 THEN BInv.TercerConteo END, 
-                CASE WHEN BInv.SegundoConteo != 0 THEN BInv.SegundoConteo END, 
+            WHEN (ISap.Cantidad * (Parte.Por / Parte.Costo)) < (COALESCE( 
+                CASE WHEN BInv.TercerConteo != 0.00 THEN BInv.TercerConteo END, 
+                CASE WHEN BInv.SegundoConteo != 0.00 THEN BInv.SegundoConteo END, 
                 BInv.PrimerConteo 
-            , 0) AND BInv.Estatus = 1 THEN (COALESCE( 
-                CASE WHEN BInv.TercerConteo != 0 THEN BInv.TercerConteo END, 
-                CASE WHEN BInv.SegundoConteo != 0 THEN BInv.SegundoConteo END, 
+            , 0.00) * (Parte.Por / Parte.Costo)) AND BInv.Estatus = 1 THEN (COALESCE( 
+                CASE WHEN BInv.TercerConteo != 0.00 THEN BInv.TercerConteo END, 
+                CASE WHEN BInv.SegundoConteo != 0.00 THEN BInv.SegundoConteo END, 
                 BInv.PrimerConteo 
-            , 0) - ISap.Cantidad) * (Parte.Por / Parte.Costo)
+            , 0.00) * (Parte.Por / Parte.Costo)) - (ISap.Cantidad * (Parte.Por / Parte.Costo))
             ELSE 0
         END
     ) AS 'Costo_Total_Positivo'
@@ -63,7 +63,7 @@ FROM
 LEFT JOIN 
     Bitacora_Inventario BInv ON ISap.GrammerNo = BInv.NumeroParte AND ISap.STBin = BInv.StorageBin
 JOIN 
-    Parte ON ISap.GrammerNo = Parte.GrammerNo");
+    Parte ON ISap.GrammerNo = Parte.GrammerNo;");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
